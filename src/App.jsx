9bf53727a98f23f8afe98d8c9880c2927efdca8b
Ram Murmu <61916324+rammurmu@users.noreply.github.com>
2022-05-07 12:08:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ class App extends React.Component {
     if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
-          placeholder="Add student..." 
+          placeholder="Add Student..." 
           currList={this.state.students}
           addFunction={this.addStudent}
           title="Student Roster"
